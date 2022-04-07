91c484050df72d78637b771db90f11523f1c5eda
german gatica <gergati1994@gmail.com>
2022-04-07 10:39:58 -0300

    Ejercicios de la clase funciones

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       suma(20, 30, 50);
+       Coche miCoche = new Coche();
+       miCoche.AgregarPuertas();
+       System.out.println("El numero total de puertas de mi auto es " + miCoche.puertas);
+    }
+    public static void suma (int a, int b, int c){
+        int otroResultado;
+        otroResultado = a + b + c;
+        String text = "La suma de los valores a + b + c : " + otroResultado;
+        System.out.println(text);
+    };
+}
+class Coche {
+    public int puertas = 3;
+
+    public void AgregarPuertas(){
+          this.puertas++;
+    };
+}
+
+
